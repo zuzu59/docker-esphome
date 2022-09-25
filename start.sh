782844f4683327f3249d6bd8371e29331552fd7d
Christian Zufferey <christian@zufferey.com>
2022-09-25 15:39:53 +0200

    new: first commit

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Petit script pour démarrer tout le binz
+#zf220207.1716
+
+#pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
+# https://github.com/zuzu59/deploy-proxmox/blob/master/install_docker.sh
+
+
+docker-compose up -d
+#docker-compose logs -f
+
+echo -e "
+
+Votre ESPhome est disponible sur:
+
+http://0.0.0.0:6052
+
+"
---
+++ stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Petit script pour arrêter tout le binz
+#zf220207.1700
+
+docker-compose stop
+
+
+
